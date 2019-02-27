b9eb06cb3a1645c43797aa0c0ce5e8287ba57d89
Nikita <nikita77725@gmail.com>
2019-02-28 00:54:26 +0300

    Реализовал чтение/запись

--- Generate_test/main.cpp
+++ Generate_test/main.cpp
@@ -1,15 +1,54 @@
 #include <iostream>
 #include <time.h>
 #include <fstream>
+#include <random>
 
 using namespace std;
 
 float* genX(int count);
-float* genY(const float a, const float b, int count);
-void write_test(const float* X,const  float* Y,const  int count);
+float* genY(const float a, const float b, float* X, int count);
+bool write_test(const string path, const float* X,const  float* Y,const  int count);
 
 int main()
 {
+    string path;
+    float a = 0, b = 0;
+    int cnt = 0;
+    cout << "Write path to save test data: ";
+    cin >> path;
+    cout << "\nWrite a: ";
+    cin >> a;
+    cout << "\nWrite b: ";
+    cin >> b;
+    cout << "\nWrite max count points: ";
+    cin >> cnt;
+
+    float* X = genX(cnt);
+    float* Y = genY(a,b, X, cnt);
+
+    cout << "X = {";
+
+    for (int i = cnt; i >= 0;i--) {
+        if(i == 0)
+            cout << X[i] << "}\n";
+        else
+            cout << X[i] <<",\t";
+    }
+    cout << "Y = {";
+
+    for (int i = cnt; i >= 0;i--) {
+        if(i == 0)
+            cout << Y[i] << "}\n";
+        else
+            cout << Y[i] <<",\t";
+    }
+
+    if(write_test(path, X, Y, cnt)){
+        cout << "\nGenerate is done\n";
+    }
+    else
+        cout << "\nSome error...\n";
+
 
     return 0;
 }
@@ -18,13 +57,13 @@ int main()
 float* genX(int count){
     float* X = new float[count];
     for (int i = count-1;i >= 0;i--) {
-        X[i] = 0.5 * (rand_r(time(NULL))%100);
+        X[i] = 0.3 * (rand()%10);
         float delta;
-        if(rand_r(time(NULL))%2 == 0)
-            delta = 0.5 * (rand_r(time(NULL))%10);
+        if(rand()%2 == 0)
+            delta = 0.3 * (rand()%10);
         else
-            delta = 0.5 * (rand_r(time(NULL))%10) * -1;
-        X[i] -= delta;
+            delta = 0.3 * (rand()%1) * -1;
+        X[i] += delta;
     }
     return X;
 }
@@ -33,16 +72,32 @@ float* genY(const float a, const float b, float* X, int count){
     float* Y = new float[count];
     for (int i = count-1;i >= 0;i--) {
         Y[i] = a*X[i] - b;
+        /*
         float delta;
-        if(rand_r(time(NULL))%2 == 0)
-            delta = 0.5 * (rand_r(time(NULL))%10);
+        if(rand()%2 == 0)
+            delta = 0.3 * (rand()%10);
         else
-            delta = 0.5 * (rand_r(time(NULL))%10) * -1;
+            delta = 0.3 * (rand()%1) * -1;
         Y[i] -= delta;
+        */
     }
+    return Y;
 }
 
-void write_test(const char* path, const float* X,const  float* Y,const  int count){
-    ofstream oFile();
+bool write_test(const string path, const float* X, const  float* Y, const  int count){
+    ofstream oFile;
 
+    oFile.open(path + "/testData.txt", ios_base::trunc | ios_base::out);
+    if(!oFile.is_open()){
+        return false;
+    }
+    oFile.write((const char*)&count, sizeof (int));
+    for (int i = count-1; i >= 0;i--) {
+        oFile.write((const char*)&X[i], sizeof (float));
+    }
+    for (int i = count-1; i >= 0;i--) {
+        oFile.write((const char*)&Y[i], sizeof (float));
+    }
+    oFile.close();
+    return true;
 }
--- Uravnenie/main.cpp
+++ Uravnenie/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -9,41 +10,49 @@ float sum_qrt(const float* mas, const int mas_count);
 float find_a(const float* masX, const float* masY, const int mas_count);
 float find_b(const float* masX, const float* masY, const int mas_count);
 
+bool readData(string path, float* X, float* Y, int* mas_count);
 
 
 
 int main()
 {
-    const int mas_count = 10;
-    float X[mas_count] = {1.2, 4.2, 3.5, 2.6, 4.7, 3.1, 3.4, 1.2, 3.9, 3.7};
-    float Y[mas_count] = {3, 12, 12, 8.9, 13, 11, 8.8, 5.2, 12.3, 10.5};
+    int mas_count = 0;
+    float* X;
+    float* Y;
+    string path;
+
+
+    cout << "Write path to test data: ";
+    cin >> path;
+
+    if(!readData(path,X,Y,&mas_count)){
+        cout << "Some error...";
+        getchar();
+        return 1;
+    }
+
+    cout << "X = {";
+
+    for (int i = mas_count; i >= 0;i--) {
+        if(i == 0)
+            cout << X[i] << "}\n";
+        else
+            cout << X[i] <<", ";
+    }
+    cout << "Y = {";
+
+    for (int i = mas_count; i >= 0;i--) {
+        if(i == 0)
+            cout << Y[i] << "}\n";
+        else
+            cout << Y[i] <<", ";
+    }
 
     cout << "a = " << find_a(X, Y, mas_count) << "\n";
     cout << "b = " << find_b(X, Y, mas_count) << "\n";
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 float sum(const float* mas, int mas_count){
@@ -82,4 +91,30 @@ float find_a(const float* masX, const float* masY, const int mas_count){
 
 float find_b(const float* masX, const float* masY, const int mas_count){
     return (sum(masX, mas_count)*sum_ad(masX, masY, mas_count) - sum_qrt(masX, mas_count)*sum(masY, mas_count))/(quart(sum(masX, mas_count))-mas_count * sum_qrt(masX, mas_count));
+}
+
+
+
+bool readData(string path, float* X, float* Y, int* mas_count){
+    ifstream iFile;
+    iFile.open(path, ios_base::in);
+    if(!iFile.is_open())
+        return false;
+    iFile.read((char*)mas_count,sizeof (int));
+    X = new float[*mas_count];
+    Y = new float[*mas_count];
+    for (int i = *mas_count-1; i >= 0;i--) {
+        if(!iFile.eof())
+            iFile.read((char*)&X[i],sizeof (float));
+        else
+            return false;
+    }
+    for (int i = *mas_count-1; i >= 0;i--) {
+        if(!iFile.eof())
+            iFile.read((char*)&Y[i],sizeof (float));
+        else
+            return false;
+    }
+    iFile.close();
+    return true;
 }
