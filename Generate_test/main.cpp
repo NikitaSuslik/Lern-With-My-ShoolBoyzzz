67634836fd1758c0ebd95f155de087e4bd08801b
Nikita <nikita77725@gmail.com>
2019-03-01 18:53:24 +0300

    Задание по экстримальному
    Добавил комментарии  и поправил некоторые недочеты

--- Generate_test/main.cpp
+++ Generate_test/main.cpp
@@ -3,67 +3,59 @@
 #include <fstream>
 #include <random>
 #include <clocale>
+#include <stdio.h>
 using namespace std;
 
-float* genX(int count);
-float* genY(const float a, const float b, float* X, int count);
-bool write_test(const char* path, const float* X,const  float* Y,const  int count);
-char *getStartPath(char* argv_0);
+float* genX(int count);                                                             //генератор Х
+float* genY(const float a, const float b, float* X, int count);                     //генератор У
+bool write_test(const char* path, const float* X,const  float* Y,const  int count); //запись тестовых данных в файл в машинном понимании
+bool write_test_txt(const char* path, const float* X, const  float* Y, const  int count, const float a, const float b); //запись тестовых данных в файл в человеческом понимании
+char *getStartPath(char* argv_0);   //получение пути до директории с исполняемым файлом
 
 int main(int argc, char *argv[])
 {
-    srand(time(nullptr));
+    srand(time(nullptr));               //настройка рандомных чисел
     setlocale(LC_ALL, "rus");
-    char* path = nullptr;
-    float a = 0, b = 0;
-    int cnt = 0;
-    char com = '\0';
+
+    char* path = nullptr;       //Создание и инициализация переменных   )
+    float a = 0, b = 0;         //                                      )
+    int cnt = 0;                //                                      )
+    char com = '\0';            //                                      )
     cout << "Выберите куда сохранить тестовые данные: 0 - в директорию с исполняемым файлом, 1 - прописать путь вручную\n";
-    while(com != '0' && com != '1'){
-        com = cin.get();
-        cin.clear();
-        if(com != '0' && com != '1'){
+    while(com != '0' && com != '1'){        //цикл чтения потока ввода, пока пользователь не введет 0 или 1
+        com = cin.get();                    //читаем один символ из потока
+        cin.clear();                        //чстим поток
+        if(com != '0' && com != '1'){       //если введено не 0 или 1 выводится предупреждение
             cout << "Выберите 0 или 1\n";
         }
     }
     switch (com) {
-    case '0':
+    case '0':                               //пользователь выбрал расположение файла в директории с исполняемым файлом
         path = getStartPath(argv[0]);
         break;
-    case '1':
+    case '1':                               //пользователь выбрал ручной ввод
         cout << "Введите путь куда сохранить тестовые данные: ";
         cin >> path;
         break;
     }
     cout << "\nВведите a: ";
-    cin >> a;
+    cin >> a;                               //введение коэффициента а
     cout << "\nВведите b: ";
-    cin >> b;
+    cin >> b;                               //введение коэффициента b
     cout << "\nВведите число точек: ";
-    cin >> cnt;
+    cin >> cnt;                             //введение количества элементов
 
-    float* X = genX(cnt);
-    float* Y = genY(a,b, X, cnt);
-
-    cout << "X = {";
-
-    for (int i = cnt; i >= 0;i--) {
-        if(i == 0)
-            cout << X[i] << "}\n";
-        else
-            cout << X[i] <<",\t";
-    }
-    cout << "Y = {";
-
-    for (int i = cnt; i >= 0;i--) {
-        if(i == 0)
-            cout << Y[i] << "}\n";
-        else
-            cout << Y[i] <<",\t";
-    }
+    float* X = genX(cnt);                   //генерируем Х
+    float* Y = genY(a,b, X, cnt);           //генерируем У
 
     if(write_test(path, X, Y, cnt)){
         cout << "\nГенерация прошла успешно\n";
+    }
+    else
+        cout << "\nОшибка при записи тестовых данных\n";
+
+    if(write_test_txt(path, X, Y, cnt, a, b)){
+        cout << "\nЧто бы ознакомится с результатами генерации пройдите по пути: \"" << path << "testData.txt";
     }
     else
         cout << "\nОшибка при записи тестовых данных\n";
@@ -99,8 +91,7 @@ bool write_test(const char *path, const float* X, const  float* Y, const  int co
     ofstream oFile;
     char tmpPath[255];
     strcpy(tmpPath, path);
-    strcat(tmpPath, "testData.test");
-    oFile.open(tmpPath, ios_base::trunc | ios_base::out);
+    oFile.open(strcat(tmpPath, "testData.test"), ios_base::trunc | ios_base::out | ios_base::binary);
     if(!oFile.is_open()){
         return false;
     }
@@ -111,14 +102,41 @@ bool write_test(const char *path, const float* X, const  float* Y, const  int co
     return true;
 }
 
+bool write_test_txt(const char* path, const float* X,const  float* Y,const  int count,const float a,const float b){
+    ofstream oFile;
+    char tmpPath[255];
+    strcpy(tmpPath, path);
+    oFile.open(strcat(tmpPath, "testData.txt"),ios_base::out | ios_base::trunc);
+    if(!oFile.is_open())
+        return false;
+    oFile << "Число элементов: " << count << "\n" ;
+    oFile << "X = {";
+    for (int i = 0; i < count;i++) {
+        if(i == 0)
+            oFile << X[i] << "}\n";
+        else
+            oFile << X[i] <<",\t";
+    }
+    oFile << "Y = {";
+
+    for (int i = 0; i < count;i++) {
+        if(i == 0)
+            oFile << Y[i] << "}\n";
+        else
+            oFile << Y[i] <<",\t";
+    }
+    oFile << "\na = " << a << "\nb = " << b;
+}
+
 char* getStartPath(char* argv_0){
-    char* path = strcpy(nullptr, argv_0);
+    char* path = new char[255];
+    strcpy(path, argv_0);
     int i = 0, last_s = 0;
     while (path[i] != '\0') {
-        if(path[i] == '/');
-            last_s = i;
+        if(path[i] == '/' || path[i] == '\\')
+            last_s = ++i;
         i++;
     }
-    path.erase(last_s);
+    path[last_s] = '\0';
     return path;
 }
--- Uravnenie/main.cpp
+++ Uravnenie/main.cpp
@@ -4,63 +4,62 @@
 
 using namespace std;
 
-float sum(const float* mas, const int mas_count);
-float quart(const float);
-float sum_ad(const float* masX, const float* masY, const int mas_count);
-float sum_qrt(const float* mas, const int mas_count);
-float find_a(const float* masX, const float* masY, const int mas_count);
-float find_b(const float* masX, const float* masY, const int mas_count);
+float sum(const float* mas, const int mas_count);                           //расчет суммы элементов в массиве
+float quart(const float);                                                   //расчет квадрата
+float sum_ad(const float* masX, const float* masY, const int mas_count);    //расчет суммы произведений двух массивов
+float sum_qrt(const float* mas, const int mas_count);                       //расчет суммы квадратов элементов
+float find_a(const float* masX, const float* masY, const int mas_count);    //поиск а
+float find_b(const float* masX, const float* masY, const int mas_count);    //поиск b
+
+bool readData(string path, float*& X, float*& Y, int* mas_count);           //чтение данных из файла по пути path
+                        //в параметрах передаются указатели в которые функция запишет указатели на массив Х, массив Y и число элементов
+char* getPathToData(const char* argv_0);                                    //определение текущей директории и добавление "testData.test"
 
 
-bool readData(string path, float*& X, float*& Y, int* mas_count);
 
-
-
-int main()
+int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "rus");
-    int mas_count = 0;
-    float* X = nullptr;
-    float* Y = nullptr;
-    string path;
+    int mas_count = 0;              //создание и инициализация переменных   )
+    float* X = nullptr;             //                                      )
+    float* Y = nullptr;             //                                      )
+    string path;                    //                                      )
+    char com = '\0';                //                                      )
 
-
-    cout << "Введите путь до файла с тестовыми данными: ";
-    cin >> path;
-    if(!readData(path,X,Y,&mas_count)){
+    cout << "Выберите откуда загрузить тестовые данные: 0 - из директории с исполняемым файлом(testData.test), 1 - прописать путь вручную\n";
+    while(com != '0' && com != '1'){        //цикл чтения потока ввода, пока пользователь не введет 0 или 1
+        com = cin.get();                    //читаем один символ из потока
+        cin.clear();                        //чстим поток
+        if(com != '0' && com != '1'){       //если введено не 0 или 1 выводится предупреждение
+            cout << "Выберите 0 или 1\n";
+        }
+    }
+    switch (com) {
+    case '0':                           //пользователь выбрал поиск файла в текущей директории
+        path = getPathToData(argv[0]);  //argv[0] хранит в себе путь до исполнямого файла
+        break;
+    case '1':                           //пользователь решил ввести путь вручную
+        cout << "Введите путь до файла с тестовыми данными: ";
+        cin >> path;
+        break;
+    }
+    if(!readData(path,X,Y,&mas_count)){ //попытка чтения из файла
         cout << "Ошибка при чтении";
         getchar();
         return 1;
     }
 
-    cout << "X = {";
-
-    for (int i = mas_count; i >= 0;i--) {
-        if(i == 0)
-            cout << X[i] << "}\n";
-        else
-            cout << X[i] <<",\t";
-    }
-    cout << "Y = {";
-
-    for (int i = mas_count; i >= 0;i--) {
-        if(i == 0)
-            cout << Y[i] << "}\n";
-        else
-            cout << Y[i] <<",\t";
-    }
-
-    cout << "a = " << find_a(X, Y, mas_count) << "\n";
-    cout << "b = " << find_b(X, Y, mas_count) << "\n";
+    cout << "a = " << find_a(X, Y, mas_count) << "\n";  //Вывод а
+    cout << "b = " << find_b(X, Y, mas_count) << "\n";  //Вывод b
 
     return 0;
 }
 
 
 float sum(const float* mas, int mas_count){
-    float res = 0;
+    float res = 0;              //Временная переменная
     for(int i = mas_count-1; i >= 0; i--){
-        res += mas[i];
+        res += mas[i];          //бегая по массиву суммируем все его элементы
     }
     return res;
 }
@@ -70,51 +69,64 @@ float quart(const float a){
 }
 
 float sum_ad(const float* masX, const float* masY, const int mas_count){
-    float res = 0;
-    float tmp_ad[mas_count];
+    float tmp_ad[mas_count];    //Временный массив с произведениями элементов
     for (int i = mas_count-1; i >= 0; i--) {
-        tmp_ad[i] = masX[i] * masY[i];
+        tmp_ad[i] = masX[i] * masY[i];  //записываем во временный массив произведения элементов двух массивов
     }
-    res = sum(tmp_ad, mas_count);
-    return res;
+    return sum(tmp_ad, mas_count);      //Возвращаем сумму всех элементов временного массива
 }
 
 float sum_qrt(const float* mas, const int mas_count){
-    float res = 0;
+    float res = 0;              //Временная переменная
     for(int i = mas_count-1; i >= 0; i--){
-        res += quart(mas[i]);
+        res += quart(mas[i]);   //бегая по массиву находим квадраты его элементов и пишем их во временную переменную
     }
     return res;
 }
 
 float find_a(const float* masX, const float* masY, const int mas_count){
+    //находим а по формуле:
+    //    sum(x)*sum(y) - n*sum(x*y)
+    //a = -------------------------
+    //    qrt(sum(x)) - n*sum(qrt(x))
     return (sum(masX, mas_count)*sum(masY, mas_count) - mas_count * sum_ad(masX, masY, mas_count))/(quart(sum(masX, mas_count))-mas_count * sum_qrt(masX, mas_count));
 }
 
 float find_b(const float* masX, const float* masY, const int mas_count){
+    //находим а по формуле:
+    //    sum(x)*sum(x*y) - sum(qrt(x))*sum(y)
+    //b = ----------------------------------
+    //        qrt(sum(x)) - n*sum(qrt(x))
     return (sum(masX, mas_count)*sum_ad(masX, masY, mas_count) - sum_qrt(masX, mas_count)*sum(masY, mas_count))/(quart(sum(masX, mas_count))-mas_count * sum_qrt(masX, mas_count));
 }
 
-bool getCountMas(string path, int* mas_count){
-    ifstream iFile;
-    iFile.open(path, ios_base::in);
-    if(!iFile.is_open())
-        return false;
-    iFile.read((char*)mas_count,sizeof (int));
-    iFile.close();
-    return true;
-}
 
 bool readData(string path, float*& X, float*& Y, int *mas_count){
     ifstream iFile;
-    iFile.open(path, ios_base::in);
+    iFile.open(path, ios_base::in); //открываем файл на чтение
     if(!iFile.is_open())
         return false;
-    iFile.read((char*)mas_count, sizeof (int));
-    X = new float[*mas_count];
-    Y = new float[*mas_count];
-    iFile.read((char*)X, sizeof(float)*(*mas_count));
+    iFile.read((char*)mas_count, sizeof (int));         //Читаем количество элементов
+    X = new float[*mas_count];                          //выделяем память подд то количество, которое прочитали
+    Y = new float[*mas_count];                          //выделяем память подд то количество, которое прочитали
+    iFile.read((char*)X, sizeof(float)*(*mas_count));   //читаем сразу весь массив Х и У размером прочитанного количества  
     iFile.read((char*)Y, sizeof(float)*(*mas_count));
-    iFile.close();
+    iFile.close();                                      
     return true;
+}
+
+char* getPathToData(const char* argv_0){
+    char* path = new char[255];
+    strcpy(path, argv_0);
+    int i = 0, last_s = 0;  
+    while (path[i] != '\0') {       //с помощью этого цикла мы отрезаем от пути название самого исполняемого файла
+        //например на входе в этот цикл в path будет "C:\\Users\nikita\Desktop\Uravnenie.exe"
+        if(path[i] == '/' || path[i] == '\\')
+            last_s = ++i;
+        i++;
+    }
+        //на выходе из цикла в path будет "C:\\Users\nikita\Desktop\"
+    strcpy(path + last_s, "testData.test");
+        //тут в  path будет "C:\\Users\nikita\Desktop\testData.test"
+    return path;
 }
